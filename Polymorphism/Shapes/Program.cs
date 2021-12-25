f269c0481b5077a7ccccb76561e59bec1aefc6fd
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2021-12-25 13:59:36 +0200

    Add exercise

---
+++ Polymorphism/Shapes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape rectangle = new Rectangle(4, 4);
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.Draw());
+
+            Shape circle = new Circle(3);
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
---
+++ Polymorphism/Shapes/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private int height;
+        private int width;
+
+        public Rectangle(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public int Height { get => height; private set => height = value; }
+        public int Width { get => width; private set => width = value; }
+
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (2 * Height) + (2 * Width);
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
