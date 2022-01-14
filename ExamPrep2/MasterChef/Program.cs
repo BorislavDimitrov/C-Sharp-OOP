82e61720e4de9e446ce9bf9dcc078aa5b88f591e
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2022-01-14 14:22:06 +0200

    Add exercise

---
+++ ExamPrep2/MasterChef/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MasterChef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input1 = Console.ReadLine().Split().Select(int.Parse).ToList();           
+            List<int> input2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Queue<int> ingredients = new Queue<int>(input1);
+            Stack<int> freshnessValues = new Stack<int>(input2);
+
+            int dippingSauce = 0;
+            int greenSalad = 0;
+            int chocolateCake = 0;
+            int lobster = 0;
+
+            while (ingredients.Count > 0 && freshnessValues.Count > 0)
+            {
+                if (ingredients.Peek() == 0)
+                {
+                    ingredients.Dequeue();
+                    continue;
+                }
+
+                int freshnessLevel = ingredients.Peek() * freshnessValues.Pop();
+
+                if (freshnessLevel == 150)
+                {
+                    ingredients.Dequeue();
+                    dippingSauce++;
+                }
+                else if (freshnessLevel == 250)
+                {
+                    ingredients.Dequeue();
+                    greenSalad++;
+                }
+                else if (freshnessLevel == 300)
+                {
+                    ingredients.Dequeue();
+                    chocolateCake++;
+                }
+                else if (freshnessLevel == 400)
+                {
+                    ingredients.Dequeue();
+                    lobster++;
+                }
+                else 
+                {
+                    int currIngre = ingredients.Dequeue() + 5;
+                    ingredients.Enqueue(currIngre);
+                }
+            }
+
+            if (dippingSauce > 0 && greenSalad > 0 && lobster > 0 && chocolateCake > 0)
+            {
+                Console.WriteLine($"Applause! The judges are fascinated by your dishes!");
+            }
+            else
+            {
+                Console.WriteLine("You were voted off. Better luck next year.");
+            }
+
+            if (ingredients.Count > 0)
+            {
+                Console.WriteLine("Ingredients left: " + ingredients.Sum());
+            }
+
+            Dictionary<string, int> dishes = new Dictionary<string, int>();
+            dishes.Add("Dipping sauce" , dippingSauce);
+            dishes.Add("Green salad" , greenSalad);
+            dishes.Add("Chocolate cake" , chocolateCake);
+            dishes.Add("Lobster" , lobster);
+            dishes = dishes.OrderBy(x => x.Key).ToDictionary(x => x.Key , y => y.Value);
+
+            foreach (var dish in dishes)
+            {
+                if (dish.Value > 0)
+                {
+                    Console.WriteLine($" # {dish.Key} --> {dish.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ ExamPrep2/Survivor/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Survivor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsNum = int.Parse(Console.ReadLine());
+            char[][] field = new char[rowsNum][];
+
+            for (int i = 0; i < rowsNum; i++)
+            {
+                field[i] = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            }
+
+            int myTokens = 0;
+            int oponentTokens = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Gong")
+                {
+                    break;
+                }
+
+                List<string> info = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+                string command = info[0];
+                int row = int.Parse(info[1]);
+                int col = int.Parse(info[2]);
+                string direction = string.Empty;
+
+                if (info.Count == 4)
+                {
+                    direction = info[3];
+                }
+
+                if (command == "Find")
+                {
+                    if (row >= 0 && row < field.Length)
+                    {
+                        if (col >= 0 && col < field[row].Length)
+                        {
+                            if (field[row][col] == 'T')
+                            {
+                                myTokens++;
+                                field[row][col] = '-';
+                            }
+                        }
+                    }
+                }
+                else if (command == "Opponent")
+                {
+                    if (row >= 0 && row < field.Length)
+                    {
+                        if (col >= 0 && col < field[row].Length)
+                        {
+                            if (field[row][col] == 'T')
+                            {
+                                oponentTokens++;
+                                field[row][col] = '-';
+
+                                if (direction == "up")
+                                {
+                                    int moveCounter = 0;
+                                    while (moveCounter < 3)
+                                    {
+                                        
+                                        if (row - 1 >= 0)
+                                        {
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
